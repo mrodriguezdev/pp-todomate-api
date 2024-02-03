da0cf70ac9f781093921026e7f9a6ee98ad58526
mrodriguezdev <marioguzman_99@hotmail.com>
2024-02-03 17:03:59 -0600

    Eliminando entidad de ejemplo. Mapeando entidad task y creando DTO

---
+++ src/main/java/mrodriguezdev/me/apitodomate/domain/model/task/TaskDTO.java
@@ -0,0 +1,10 @@
+package mrodriguezdev.me.apitodomate.domain.model.task;
+
+public class TaskDTO {
+    public Long id;
+    public String title;
+    public String description;
+    public String creationDate;
+    public String dueDate;
+    public Boolean completed;
+}
