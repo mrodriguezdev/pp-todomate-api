1502b129b59643d2bcc4333c96c3135484e74eaa
mrodriguezdev <marioguzman_99@hotmail.com>
2024-02-03 16:12:14 -0600

    first commit

---
+++ src/test/java/mrodriguezdev/me/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package mrodriguezdev.me;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
